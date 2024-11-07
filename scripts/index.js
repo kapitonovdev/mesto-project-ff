8150d8ceb0b9d5ab7f2aaad4fa1d5cd214d3884c
Pavel Kapitonov <kapitonovdev@icloud.com>
2024-11-08 01:01:17 +0300

    Added card creation and deletion functionality with template cloning

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,42 @@
-// @todo: Темплейт карточки
+// Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content;
 
-// @todo: DOM узлы
+// DOM узлы
+const placesList = document.querySelector('.places__list');
 
-// @todo: Функция создания карточки
+// Функция создания карточки
+function createCard(cardData, deleteCard) {
+  // Клонируем шаблон
+  const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
 
-// @todo: Функция удаления карточки
+  // Устанавливаем значения вложенных элементов
+  const cardImage = cardElement.querySelector('.card__image');
+  const cardTitle = cardElement.querySelector('.card__title');
+  const deleteButton = cardElement.querySelector('.card__delete-button');
 
-// @todo: Вывести карточки на страницу
+  // Устанавливаем изображение и заголовок
+  cardImage.src = cardData.link;
+  cardImage.alt = cardData.name;
+  cardTitle.textContent = cardData.name;
+
+  // Добавляем обработчик для удаления карточки
+  deleteButton.addEventListener('click', () => deleteCard(cardElement));
+
+  return cardElement;
+}
+
+// Функция удаления карточки
+function deleteCard(cardElement) {
+  cardElement.remove();
+}
+
+// Выводим карточки на страницу
+function renderCards(cardsData) {
+  cardsData.forEach(cardData => {
+    const cardElement = createCard(cardData, deleteCard);
+    placesList.append(cardElement);
+  });
+}
+
+// Вызов функции для отображения всех карточек из массива initialCards
+renderCards(initialCards);
